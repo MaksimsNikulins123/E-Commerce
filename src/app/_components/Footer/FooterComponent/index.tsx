097f2ab689a4a010987a6f3169d0fb18e15e36e4
Max <nikulins.maksims@gmail.com>
2024-02-16 16:09:27 +0200

    Footer is ready

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -10,13 +10,14 @@ import { Gutter } from '../../Gutter'
 import classes from './index.module.scss'
 import Image from 'next/image'
 import Link from 'next/link'
-import { Footer } from '../../../../payload/payload-types'
+import { Footer, Media } from '../../../../payload/payload-types'
+import { Button } from '../../Button'
 
 const FooterComponent = ({ footer}: { footer: Footer}) => {
 
   const pathname = usePathname()
 
-  // const navItems = footer?.navItems || []
+  const navItems = footer?.navItems || []
   return (
     <footer
       className={
@@ -62,8 +63,30 @@ const FooterComponent = ({ footer}: { footer: Footer}) => {
               />
             </Link>
             <p>{footer.copyright}</p>
+            <div className={classes.socialLinks}>
+            {navItems.map(item => {
+                const icon = item?.link?.icon as Media;
 
-            
+                return (
+                  <Button
+                    key={item.link.label}
+                    el='link'
+                    href={item.link.url}
+                    newTab={true}
+                    className={classes.socialLinkItem}
+
+                  >
+                    <Image 
+                      src={icon?.url}
+                      alt={item.link.label}
+                      width={24}
+                      height={24}
+                      className={classes.socialIcon}
+                    />
+                  </Button>
+                )
+              })}
+            </div>
           </div>
         </Gutter>
       </div>
